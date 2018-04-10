67447cb59b6a335d9c8ad0e4fc8bfe17a663fd98
Olga <olgapoddubko@gmail.com>
2018-04-10 16:19:25 +0300

    edited server routers

--- src/server/routers/blog.js
+++ src/server/routers/blog.js
@@ -4,49 +4,38 @@ const router = express.Router();
 const Article = require('../mongoose/models/article');
 
 router.get('/', function(req, res, next) {
-  Article.find({}, function(err, blog) {
+  Article.find(function (err, blog) {
+    if (err) return next(err);
     res.send(blog);
   });
 });
 
-router.post('/add', function(req, res, next) {
-  const article = new Article({
-    id: req.body.id,
-    author: req.body.author,
-    title: req.body.title,
-    text: req.body.text
-  });
-  article.save(function(err) { // r
-    if (err) return next(err);
-    res.send('added a new article');
-  });
-});
-
 router.get('/:id', function(req, res, next) {
-  Article.find({ id: req.params._id }, function(err, blog) {
-    if (!blog.length) {
-      return next(err);
-    }
-    res.render('article', { blog });
+  Article.findById(req.params.id, function (err, article) {
+      if (err) return next(err);
+      res.send(article);
+    });
+});
+
+router.post('/add', function(req, res, next) {
+  Article.create(req.body, function (err, article) {
+     if (err) return next(err);
+     res.send(article);
+   });
+});
+
+router.put('/:id', function(req, res, next) {
+  Article.findByIdAndUpdate(req.params.id, req.body, function (err, article) {
+    if (err) return next(err);
+    res.send(article);
   });
 });
 
-router.post('/:id', function(req, res, next) {
-  if (req.body._method === 'put') {
-    Article.update(
-      { id: req.params.id },
-      { title: req.body.title, text: req.body.text },
-      function(err, raw) {
-        if (err) return next(err);
-        res.send('an article have been updated');
-      },
-    );
-  } else if (req.body._method === 'delete') {
-    Article.find({ id: req.params.id }).remove(function(err) {// r
-      if (err) return next(err);
-      res.send('an article have been deleted');
-    });
-  }
+router.delete('/:id', function(req, res, next) {
+  Article.findByIdAndRemove(req.params.id, req.body, function (err, article) {
+    if (err) return next(err);
+    res.send(article);
+  });
 });
 
 module.exports = router;
